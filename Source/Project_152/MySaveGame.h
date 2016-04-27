a42e88581e45c1ab2066b4cb6803a9eb8cd84984
clintfresnostate <clinthatayama@mail.fresnostate.edu>
2016-04-26 21:32:29 -0700

    Fixed Merge Bugs

--- Source/Project_152/MySaveGame.h
+++ Source/Project_152/MySaveGame.h
@@ -19,11 +19,9 @@ public:
 		AProject_152Character* mainPlayer;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Basic)
-<<<<<<< HEAD
+
 		FString SaveSlotName = "TEST";
-=======
-		FString SaveSlotName = "test";
->>>>>>> origin/master
+
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Basic)
 		int32 UserIndex = 0;
